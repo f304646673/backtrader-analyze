860538f8190d25986396166217e0ce94428f4a17
fangliang <f304646673@gmail.com>
2024-09-26 16:45:38 +0800

    Refactor strategy code for better readability and maintainability

--- samples/bracket/bracket.py
+++ samples/bracket/bracket.py
@@ -30,15 +30,15 @@ import backtrader as bt
 
 class St(bt.Strategy):
     params = dict(
-        ma=bt.ind.SMA,
-        p1=5,
-        p2=15,
-        limit=0.005,
-        limdays=3,
-        limdays2=1000,
-        hold=10,
-        usebracket=False,  # use order_target_size
-        switchp1p2=False,  # switch prices of order1 and order2
+        ma=bt.ind.SMA,  # 移动平均线指标
+        p1=5,   # 短周期
+        p2=15,  # 长周期
+        limit=0.005,    # 限价单的价格偏移
+        limdays=3,  # 限价单的有效期
+        limdays2=1000,  # 止损单和止盈单的有效期
+        hold=10,    # 持有时间
+        usebracket=False,  # use order_target_size  使用 order_target_size
+        switchp1p2=False,  # switch prices of order1 and order2 切换 order1 和 order2 的价格
     )
 
     def notify_order(self, order):
@@ -47,148 +47,148 @@ class St(bt.Strategy):
             order.ref, 'Buy' * order.isbuy() or 'Sell',
             order.getstatusname()))
 
-        if order.status == order.Completed:
-            self.holdstart = len(self)
+        if order.status == order.Completed: # 如果订单完成
+            self.holdstart = len(self)  # 记录持有开始的时间
 
-        if not order.alive() and order.ref in self.orefs:
-            self.orefs.remove(order.ref)
+        if not order.alive() and order.ref in self.orefs:   # 如果订单不活跃且订单的 ref 在 self.orefs 中
+            self.orefs.remove(order.ref)    # 从 self.orefs 中移除订单的 ref
 
     def __init__(self):
-        ma1, ma2 = self.p.ma(period=self.p.p1), self.p.ma(period=self.p.p2)
-        self.cross = bt.ind.CrossOver(ma1, ma2)
+        ma1, ma2 = self.p.ma(period=self.p.p1), self.p.ma(period=self.p.p2) # 创建两个移动平均线指标
+        self.cross = bt.ind.CrossOver(ma1, ma2)   # 创建交叉指标
 
-        self.orefs = list()
+        self.orefs = list()   # 订单列表
 
         if self.p.usebracket:
             print('-' * 5, 'Using buy_bracket')
 
     def next(self):
-        if self.orefs:
-            return  # pending orders do nothing
+        if self.orefs:  # 如果有订单
+            return  # pending orders do nothing 挂单不做任何操作
 
-        if not self.position:
-            if self.cross > 0.0:  # crossing up
+        if not self.position: # 如果没有持仓
+            if self.cross > 0.0:  # crossing up 交叉上穿
 
-                close = self.data.close[0]
-                p1 = close * (1.0 - self.p.limit)
-                p2 = p1 - 0.02 * close
-                p3 = p1 + 0.02 * close
+                close = self.data.close[0]  # 当前的收盘价
+                p1 = close * (1.0 - self.p.limit)   # 限价单的价格
+                p2 = p1 - 0.02 * close  # 止损单的价格
+                p3 = p1 + 0.02 * close  # 止盈单的价格
 
-                valid1 = datetime.timedelta(self.p.limdays)
-                valid2 = valid3 = datetime.timedelta(self.p.limdays2)
+                valid1 = datetime.timedelta(self.p.limdays) # 限价单的有效期
+                valid2 = valid3 = datetime.timedelta(self.p.limdays2)   # 止损单和止盈单的有效期
 
-                if self.p.switchp1p2:
-                    p1, p2 = p2, p1
-                    valid1, valid2 = valid2, valid1
+                if self.p.switchp1p2:   # 切换 order1 和 order2 的价格
+                    p1, p2 = p2, p1 # 切换 order1 和 order2 的价格
+                    valid1, valid2 = valid2, valid1 # 切换 order1 和 order2 的有效期
 
-                if not self.p.usebracket:
-                    o1 = self.buy(exectype=bt.Order.Limit,
-                                  price=p1,
-                                  valid=valid1,
-                                  transmit=False)
+                if not self.p.usebracket:   # 如果不使用 order_target_size
+                    o1 = self.buy(exectype=bt.Order.Limit,  # 买入限价单
+                                  price=p1, # 价格
+                                  valid=valid1, # 有效期
+                                  transmit=False)   # 不立即传输
 
                     print('{}: Oref {} / Buy at {}'.format(
                         self.datetime.date(), o1.ref, p1))
 
-                    o2 = self.sell(exectype=bt.Order.Stop,
-                                   price=p2,
-                                   valid=valid2,
-                                   parent=o1,
-                                   transmit=False)
+                    o2 = self.sell(exectype=bt.Order.Stop,  # 卖出止损单
+                                   price=p2,    # 价格
+                                   valid=valid2,    # 有效期
+                                   parent=o1,   # 父订单
+                                   transmit=False)  # 不立即传输
 
                     print('{}: Oref {} / Sell Stop at {}'.format(
                         self.datetime.date(), o2.ref, p2))
 
-                    o3 = self.sell(exectype=bt.Order.Limit,
-                                   price=p3,
-                                   valid=valid3,
-                                   parent=o1,
-                                   transmit=True)
+                    o3 = self.sell(exectype=bt.Order.Limit, # 卖出止盈单
+                                   price=p3,    # 价格
+                                   valid=valid3,    # 有效期
+                                   parent=o1,   # 父订单
+                                   transmit=True)   # 立即传输
 
                     print('{}: Oref {} / Sell Limit at {}'.format(
                         self.datetime.date(), o3.ref, p3))
 
-                    self.orefs = [o1.ref, o2.ref, o3.ref]
+                    self.orefs = [o1.ref, o2.ref, o3.ref]   # 记录订单的 ref
 
                 else:
                     os = self.buy_bracket(
-                        price=p1, valid=valid1,
-                        stopprice=p2, stopargs=dict(valid=valid2),
-                        limitprice=p3, limitargs=dict(valid=valid3),)
+                        price=p1, valid=valid1,  # 买入限价单
+                        stopprice=p2, stopargs=dict(valid=valid2),  # 卖出止损单
+                        limitprice=p3, limitargs=dict(valid=valid3),)   # 卖出止盈单
 
-                    self.orefs = [o.ref for o in os]
+                    self.orefs = [o.ref for o in os]    # 记录订单的 ref
 
         else:  # in the market
-            if (len(self) - self.holdstart) >= self.p.hold:
+            if (len(self) - self.holdstart) >= self.p.hold: # 持有时间超过指定时间
                 pass  # do nothing in this case
 
 
 def runstrat(args=None):
     args = parse_args(args)
 
-    cerebro = bt.Cerebro()
+    cerebro = bt.Cerebro()  # 创建 Cerebro 引擎
 
     # Data feed kwargs
     kwargs = dict()
 
     # Parse from/to-date
     dtfmt, tmfmt = '%Y-%m-%d', 'T%H:%M:%S'
-    for a, d in ((getattr(args, x), x) for x in ['fromdate', 'todate']):
-        if a:
-            strpfmt = dtfmt + tmfmt * ('T' in a)
-            kwargs[d] = datetime.datetime.strptime(a, strpfmt)
+    for a, d in ((getattr(args, x), x) for x in ['fromdate', 'todate']):    # 获取 fromdate 和 todate
+        if a:   # 如果存在
+            strpfmt = dtfmt + tmfmt * ('T' in a)    # 日期时间格式
+            kwargs[d] = datetime.datetime.strptime(a, strpfmt)  # 将字符串转换为日期时间对象
 
     # Data feed
-    data0 = bt.feeds.BacktraderCSVData(dataname=args.data0, **kwargs)
-    cerebro.adddata(data0)
+    data0 = bt.feeds.BacktraderCSVData(dataname=args.data0, **kwargs)   # 创建数据源
+    cerebro.adddata(data0)  # 将数据添加到 Cerebro 引擎
 
     # Broker
-    cerebro.broker = bt.brokers.BackBroker(**eval('dict(' + args.broker + ')'))
+    cerebro.broker = bt.brokers.BackBroker(**eval('dict(' + args.broker + ')')) # 创建 Broker，参数从命令行中获取
 
     # Sizer
-    cerebro.addsizer(bt.sizers.FixedSize, **eval('dict(' + args.sizer + ')'))
+    cerebro.addsizer(bt.sizers.FixedSize, **eval('dict(' + args.sizer + ')'))   # 创建 Sizer，参数从命令行中获取
 
     # Strategy
-    cerebro.addstrategy(St, **eval('dict(' + args.strat + ')'))
+    cerebro.addstrategy(St, **eval('dict(' + args.strat + ')'))   # 创建策略，参数从命令行中获取
 
     # Execute
-    cerebro.run(**eval('dict(' + args.cerebro + ')'))
+    cerebro.run(**eval('dict(' + args.cerebro + ')'))   # 运行策略
 
-    if args.plot:  # Plot if requested to
+    if args.plot:  # Plot if requested to   如果请求绘图
         cerebro.plot(**eval('dict(' + args.plot + ')'))
 
 
 def parse_args(pargs=None):
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter, # 参数默认值帮助格式
         description=(
             'Sample Skeleton'
         )
     )
 
-    parser.add_argument('--data0', default='../../datas/2005-2006-day-001.txt',
+    parser.add_argument('--data0', default='../../datas/2005-2006-day-001.txt', # 数据文件路径
                         required=False, help='Data to read in')
 
     # Defaults for dates
-    parser.add_argument('--fromdate', required=False, default='',
+    parser.add_argument('--fromdate', required=False, default='',   # 起始日期
                         help='Date[time] in YYYY-MM-DD[THH:MM:SS] format')
 
-    parser.add_argument('--todate', required=False, default='',
+    parser.add_argument('--todate', required=False, default='', # 结束日期
                         help='Date[time] in YYYY-MM-DD[THH:MM:SS] format')
 
-    parser.add_argument('--cerebro', required=False, default='',
+    parser.add_argument('--cerebro', required=False, default='',    # Cerebro 参数
                         metavar='kwargs', help='kwargs in key=value format')
 
-    parser.add_argument('--broker', required=False, default='',
+    parser.add_argument('--broker', required=False, default='', # Broker 参数
                         metavar='kwargs', help='kwargs in key=value format')
 
-    parser.add_argument('--sizer', required=False, default='',
+    parser.add_argument('--sizer', required=False, default='',  # Sizer 参数
                         metavar='kwargs', help='kwargs in key=value format')
 
-    parser.add_argument('--strat', required=False, default='',
+    parser.add_argument('--strat', required=False, default='',  # 策略参数
                         metavar='kwargs', help='kwargs in key=value format')
 
-    parser.add_argument('--plot', required=False, default='',
+    parser.add_argument('--plot', required=False, default='',   # 绘图参数
                         nargs='?', const='{}',
                         metavar='kwargs', help='kwargs in key=value format')
 
--- samples/btfd/btfd.py
+++ samples/btfd/btfd.py
@@ -33,56 +33,56 @@ import backtrader as bt
 
 class ValueUnlever(bt.observers.Value):
     '''Extension of regular Value observer to add leveraged view'''
-    lines = ('value_lever', 'asset')
-    params = (('assetstart', 100000.0), ('lever', True),)
+    lines = ('value_lever', 'asset')    # 添加 value_lever 和 asset 两个线
+    params = (('assetstart', 100000.0), ('lever', True),)   # 添加 assetstart 和 lever 两个参数
 
     def next(self):
-        super(ValueUnlever, self).next()
-        if self.p.lever:
-            self.lines.value_lever[0] = self._owner.broker._valuelever
+        super(ValueUnlever, self).next()    # 调用父类的 next 方法
+        if self.p.lever:    
+            self.lines.value_lever[0] = self._owner.broker._valuelever  # 计算杠杆价值
 
         if len(self) == 1:
-            self.lines.asset[0] = self.p.assetstart
+            self.lines.asset[0] = self.p.assetstart # 设置资产初始值
         else:
-            change = self.data[0] / self.data[-1]
-            self.lines.asset[0] = change * self.lines.asset[-1]
+            change = self.data[0] / self.data[-1]   # 计算资产变化
+            self.lines.asset[0] = change * self.lines.asset[-1] # 计算资产
 
 
 class St(bt.Strategy):
     params = (
-        ('fall', -0.01),
-        ('hold', 2),
-        ('approach', 'highlow'),
-        ('target', 1.0),
-        ('prorder', False),
-        ('prtrade', False),
-        ('prdata', False),
+        ('fall', -0.01),    # 下跌幅度
+        ('hold', 2),    # 持有时间
+        ('approach', 'highlow'),    # 逼近方式
+        ('target', 1.0),    # 目标
+        ('prorder', False),   # 是否打印订单
+        ('prtrade', False),  # 是否打印交易
+        ('prdata', False),  # 是否打印数据
     )
 
     def __init__(self):
-        if self.p.approach == 'closeclose':
-            self.pctdown = self.data.close / self.data.close(-1) - 1.0
-        elif self.p.approach == 'openclose':
-            self.pctdown = self.data.close / self.data.open - 1.0
-        elif self.p.approach == 'highclose':
-            self.pctdown = self.data.close / self.data.high - 1.0
-        elif self.p.approach == 'highlow':
-            self.pctdown = self.data.low / self.data.high - 1.0
+        if self.p.approach == 'closeclose':   # 如果逼近方式为 closeclose
+            self.pctdown = self.data.close / self.data.close(-1) - 1.0  # 计算下跌幅度，即收盘价与前一天收盘价的比值减 1
+        elif self.p.approach == 'openclose':    # 如果逼近方式为 openclose
+            self.pctdown = self.data.close / self.data.open - 1.0   # 计算下跌幅度，即收盘价与开盘价的比值减 1
+        elif self.p.approach == 'highclose':    # 如果逼近方式为 highclose
+            self.pctdown = self.data.close / self.data.high - 1.0   # 计算下跌幅度，即收盘价与最高价的比值减 1
+        elif self.p.approach == 'highlow':  # 如果逼近方式为 highlow
+            self.pctdown = self.data.low / self.data.high - 1.0 # 计算下跌幅度，即最低价与最高价的比值减 1
 
     def next(self):
-        if self.position:
-            if len(self) == self.barexit:
-                self.close()
+        if self.position:   # 如果有持仓
+            if len(self) == self.barexit:   # 如果持有时间达到 hold
+                self.close()    # 平仓
                 if self.p.prdata:
-                    print(','.join(str(x) for x in
+                    print(','.join(str(x) for x in  
                                    ['DATA', 'CLOSE',
                                     self.data.datetime.date().isoformat(),
                                     self.data.close[0],
                                     float('NaN')]))
         else:
-            if self.pctdown <= self.p.fall:
-                self.order_target_percent(target=self.p.target)
-                self.barexit = len(self) + self.p.hold
+            if self.pctdown <= self.p.fall: # 如果下跌幅度小于 fall
+                self.order_target_percent(target=self.p.target)   # 买入目标百分比
+                self.barexit = len(self) + self.p.hold  # 记录持有时间
 
                 if self.p.prdata:
                     print(','.join(str(x) for x in
@@ -102,9 +102,9 @@ class St(bt.Strategy):
             print(','.join(['DATA', 'Action', 'Date', 'Price', 'PctDown']))
 
     def notify_order(self, order):
-        if order.status in [order.Margin, order.Rejected, order.Canceled]:
+        if order.status in [order.Margin, order.Rejected, order.Canceled]:  # 如果订单状态为保证金、拒绝或取消
             print('ORDER FAILED with status:', order.getstatusname())
-        elif order.status == order.Completed:
+        elif order.status == order.Completed:   # 如果订单状态为完成
             if self.p.prorder:
                 print(','.join(map(str, [
                     'ORDER', 'BUY' * order.isbuy() or 'SELL',
@@ -119,22 +119,22 @@ class St(bt.Strategy):
         if not self.p.prtrade:
             return
 
-        if trade.isclosed:
+        if trade.isclosed:  # 如果交易已关闭
             print(','.join(map(str, [
                 'TRADE', 'CLOSE',
-                self.data.num2date(trade.dtclose).date().isoformat(),
-                trade.value,
-                trade.pnl,
-                trade.commission,
+                self.data.num2date(trade.dtclose).date().isoformat(),   # 交易关闭的日期
+                trade.value,    # 交易价值
+                trade.pnl,  # 交易盈亏
+                trade.commission,   # 交易佣金
             ]
             )))
-        elif trade.justopened:
+        elif trade.justopened:  # 如果交易刚刚打开
             print(','.join(map(str, [
                 'TRADE', 'OPEN',
-                self.data.num2date(trade.dtopen).date().isoformat(),
-                trade.value,
-                trade.pnl,
-                trade.commission,
+                self.data.num2date(trade.dtopen).date().isoformat(),    # 交易打开的日期
+                trade.value,    # 交易价值
+                trade.pnl,  # 交易盈亏
+                trade.commission,   # 交易佣金
             ]
             )))
 
@@ -152,32 +152,32 @@ def runstrat(args=None):
     for a, d in ((getattr(args, x), x) for x in ['fromdate', 'todate']):
         kwargs[d] = datetime.datetime.strptime(a, dtfmt + tmfmt * ('T' in a))
 
-    if not args.offline:
-        YahooData = bt.feeds.YahooFinanceData
+    if not args.offline:    # 如果不使用离线文件
+        YahooData = bt.feeds.YahooFinanceData   # 使用 YahooFinanceData
     else:
-        YahooData = bt.feeds.YahooFinanceCSVData
+        YahooData = bt.feeds.YahooFinanceCSVData    # 使用 YahooFinanceCSVData
 
     # Data feed - no plot - observer will do the job
-    data = YahooData(dataname=args.data, plot=False, **kwargs)
-    cerebro.adddata(data)
+    data = YahooData(dataname=args.data, plot=False, **kwargs)  # 创建数据源
+    cerebro.adddata(data)   # 将数据添加到 Cerebro 引擎
 
     # Broker
-    cerebro.broker = bt.brokers.BackBroker(**eval('dict(' + args.broker + ')'))
+    cerebro.broker = bt.brokers.BackBroker(**eval('dict(' + args.broker + ')')) # 创建 Broker，参数从命令行中获取
 
     # Add a commission
-    cerebro.broker.setcommission(**eval('dict(' + args.comminfo + ')'))
+    cerebro.broker.setcommission(**eval('dict(' + args.comminfo + ')')) # 设置手续费
 
     # Strategy
-    cerebro.addstrategy(St, **eval('dict(' + args.strat + ')'))
+    cerebro.addstrategy(St, **eval('dict(' + args.strat + ')'))  # 添加策略
 
     # Add specific observer
-    cerebro.addobserver(ValueUnlever, **eval('dict(' + args.valobserver + ')'))
+    cerebro.addobserver(ValueUnlever, **eval('dict(' + args.valobserver + ')'))   # 添加 ValueUnlever 观察者
 
     # Execute
-    cerebro.run(**eval('dict(' + args.cerebro + ')'))
+    cerebro.run(**eval('dict(' + args.cerebro + ')'))   # 运行策略
 
     if args.plot:  # Plot if requested to
-        cerebro.plot(**eval('dict(' + args.plot + ')'))
+        cerebro.plot(**eval('dict(' + args.plot + ')'))  # 绘制图表
 
 
 def parse_args(pargs=None):
@@ -190,39 +190,39 @@ def parse_args(pargs=None):
              'can_anyone_replicate_this_strategy/')]))
         )
 
-    parser.add_argument('--offline', required=False, action='store_true',
+    parser.add_argument('--offline', required=False, action='store_true',   # 是否使用离线文件
                         help='Use offline file with ticker name')
 
-    parser.add_argument('--data', required=False, default='^GSPC',
+    parser.add_argument('--data', required=False, default='^GSPC',  # 数据文件
                         metavar='TICKER', help='Yahoo ticker to download')
 
-    parser.add_argument('--fromdate', required=False, default='1990-01-01',
+    parser.add_argument('--fromdate', required=False, default='1990-01-01',   # 起始日期
                         metavar='YYYY-MM-DD[THH:MM:SS]',
                         help='Starting date[time]')
 
-    parser.add_argument('--todate', required=False, default='2016-10-01',
+    parser.add_argument('--todate', required=False, default='2016-10-01',   # 结束日期
                         metavar='YYYY-MM-DD[THH:MM:SS]',
                         help='Ending date[time]')
 
-    parser.add_argument('--cerebro', required=False, default='stdstats=False',
+    parser.add_argument('--cerebro', required=False, default='stdstats=False',  # Cerebro 参数
                         metavar='kwargs', help='kwargs in key=value format')
 
-    parser.add_argument('--broker', required=False,
+    parser.add_argument('--broker', required=False,     # Broker 参数
                         default='cash=100000.0, coc=True',
                         metavar='kwargs', help='kwargs in key=value format')
 
-    parser.add_argument('--valobserver', required=False,
+    parser.add_argument('--valobserver', required=False,    # 观察者参数
                         default='assetstart=100000.0',
                         metavar='kwargs', help='kwargs in key=value format')
 
-    parser.add_argument('--strat', required=False,
+    parser.add_argument('--strat', required=False,  # 策略参数
                         default='approach="highlow"',
                         metavar='kwargs', help='kwargs in key=value format')
 
-    parser.add_argument('--comminfo', required=False, default='leverage=2.0',
+    parser.add_argument('--comminfo', required=False, default='leverage=2.0',   # 手续费参数
                         metavar='kwargs', help='kwargs in key=value format')
 
-    parser.add_argument('--plot', required=False, default='',
+    parser.add_argument('--plot', required=False, default='',   # 绘图参数
                         nargs='?', const='volume=False',
                         metavar='kwargs', help='kwargs in key=value format')
 
